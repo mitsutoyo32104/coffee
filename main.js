8e3a6a5af9068bdbd7d87ec728bbaaa33ea5d168
Mitsu32104 <mitsutoyo32104@gmail.com>
2023-07-07 14:51:36 +0900

    reset code

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', function(){
-   const els = document.querySelectorAll('.header-nav__link');
-   console.log(els);
-   els.forEach(el => {
-      el.addEventListener('mouseover', () => el.classList.add('change-color'));
-      el.addEventListener('mouseout', () => el.classList.remove('change-color'));
-   });
-});
+
